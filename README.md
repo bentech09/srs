7e2f7b1df8a08abc97d0482175a5e94d8e7eb7fe
ben <ben@example.com>
2025-06-16 23:40:13 +0200

    index.php parse settings.ini

---
+++ README.md
@@ -0,0 +1,3 @@
+1.Create a copy of settings.template.ini to settings.ini and fill in the necessary absolute paths
+
+2.In production environment set the settings.ini to read-only.
---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+  /* parse settings */
+    $configPath = __DIR__ . '\settings.ini';
+    if (!file_exists($configPath)) {
+      die("Not found: settings.ini");
+    }
+    
+    $config = parse_ini_file($configPath, true);
+    if ($config === false) {
+      die("Cannot read: settings.ini");
+    }
+  /* parse settings */
+
+  session_start();
+
+?>
